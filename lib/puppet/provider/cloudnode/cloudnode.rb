5fac5a56c5c02750a535532f99bdba1d24db6f39
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2011-10-04 19:06:22 +0200

    Remove old comments

--- lib/puppet/provider/cloudnode/cloudnode.rb
+++ lib/puppet/provider/cloudnode/cloudnode.rb
@@ -2,22 +2,6 @@ require 'puppet/cloudpack'
 Puppet::Type.type(:cloudnode).provide(:cloudnode) do
     desc "Puppet CloudPack provider."
 
-    # def manager
-    #     self.class.manager
-    # end
-    # 
-    # def self.prefetch(resources)
-    #     resources.each do |name, resource|
-    #         puts resource.inspect
-    #         # result[:ensure] = :present
-    #         # puts result.inspect
-    #         # resource.provider = new(result)
-    #     end
-    # 
-    # 
-    # 
-    # end
-
     def create
         resource_tags = @resource[:tags]
         name_tag = {"Name" => @resource[:name]}
